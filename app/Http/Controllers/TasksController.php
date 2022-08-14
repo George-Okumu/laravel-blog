145a9f48e042a812ed866de7c9bd87304f032cc1
George-Okumu <gokumu12@gmail.com>
2022-08-14 10:55:40 +0300

    model route binding through a wild card, in show method

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -14,9 +14,12 @@ class TasksController extends Controller
         return view('welcome', compact('tasks'));
     }
 
-    public function show($id)
+    public function show(Task $task)
     {
-        $task = Task::find($id);
+        //Task $task This is known as wild card, (Route model binding.), The name should be the same as the one from the routes:
+        //In my case it was task/{task},
+        // This replaces the below task
+        // $task = Task::findorFail($id);
         // dd($id);
 
         return view('singleTask', compact('task'));
